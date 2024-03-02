90c73b8765fabc3a161cdcec9bad68a113c235c1
leatherbullet <al.obnizov@yandex.ru>
2024-03-02 10:58:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char userSign;
+            string signs = "";
+            int numberOfSign;
+
+            Console.WriteLine("введите имя");
+            name = Console.ReadLine();
+            Console.WriteLine("введите символ");
+            userSign = Console.ReadKey().KeyChar;
+
+            string signNameSing = userSign + name + userSign;
+            numberOfSign = signNameSing.Length;
+
+            for (int i = 0; i < numberOfSign; i++)
+            {
+                signs += userSign;
+            }
+            Console.Clear();
+            Console.WriteLine(signs);
+            Console.WriteLine(signNameSing);
+            Console.WriteLine(signs);
+        }
+    }
+}
